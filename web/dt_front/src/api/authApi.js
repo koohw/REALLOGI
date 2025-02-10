394e4421a0da1e9968ee272d295bfc92c7956d2d
김지홍 <rokepj2696@gmail.com>
2025-02-10 15:57:25 +0900

    fix:url

--- web/dt_front/src/api/authApi.js
+++ web/dt_front/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL ='http://localhost:8080/api';
-console.log('Base URL:', BASE_URL);
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+
 
 // Create axios instance with default config
 const apiClient = axios.create({
